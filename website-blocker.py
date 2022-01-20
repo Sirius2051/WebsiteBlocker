1ae6a2de5dbf38e89ad17cbd3e81cd16e6c2a717
José Alejandro Avilé <sirius2051@protonmail.ch>
2022-01-20 13:19:48 -0400

    Update Main Script

--- website-blocker.py
+++ website-blocker.py
@@ -1,69 +1,68 @@
-#!/usr/bin/python
-#
-# You need run the script as root.
-#
-# Import the module 'os' to consult the operating system that we are using.
-#
 import os
-#
-# Make the query mentioned in the comment above and save it in a variable for later use.
-#
-os_name = os.name
-#
-# Print a welcome message to the user on the screen. The text is painted orange.
-#
-print('\033[;33m'+'Enter a URL or write \'ok\' to close the program.')
-#
-# Create an empty list in which the websites that the user will enter.
-#
-websites_list = []
-#
-# Define the 'input_control()' function.
-# Controls data entry to store in the list created for websites that the user wants to block.
-# It also allows you to exit the process by typing 'ok'.
-# The information entered by the user will be painted purple.
-#
-def input_control():
-    qwerty = input('\033[;35m'+'\t')
-    if qwerty == 'ok':
-        pass
-    else:
-        websites_list.append(qwerty)
-        input_control() 
-#
-# Start the execution of the function 'input_control()'.
-#
-input_control()
-#
-# Define function 'blocker()'.
-# This function will open the 'hosts' file and write to it the introduced websites
-# to redirect to the 'localhost' every time you try to access any of them.
-# The receiving parameter will allow you to pass the location of the 'hosts' file taking into account the operating system.
-#
-def blocker(route):
-    with open(route, 'r+') as file:
-        content = file.read()
-        for website in websites_list:
-            if website in content:
-                pass
-            else:
-                line = '127.0.0.1 \t'+website+'\n'
-                file.write(line)
-#
-# Checking the operating system and based on that we pass the path to the 'blocker()' function to execute the website blocking.
-#
-if os_name == 'posix':
-    route = '/etc/hosts'
-    blocker(route)
-elif os_name == 'nt':
-    route = 'C:\Windows\System32\drivers\etc\hosts';
-    blocker(route)
-else:
-    print('\033[;31m'+'Your operating system is not supported. Please contact us.')
-#
-# Print a farewell message that indicates the status of the operations.
-#
-if len(websites_list) > 0:
-    print('\033[;32m'+'Operations successfully completed.')
-else:
-    print('\033[;31m'+'You have not indicated any website.')
+
+class WebsiteBlocker:
+    def __init__(self):
+        self.os_name = os.name
+        self.websites = []
+        self.route = ""
+
+    def Main(self):
+        self.Route()
+        self.InputControl()
+        self.Blocker()
+        if len(self.websites) > 0:
+            print('\033[;32mOperations successfully completed.')
+        else:
+            print('\033[;31m'+'You have not indicated any website.')
+
+    def InputControl(self):
+        text = input('\033[;35m\t')
+        if text == 'ok':
+            pass
+        else:
+            self.websites.append(text)
+            self.InputControl() 
+
+    def Blocker(self):
+        with open(self.route, 'r+') as file:
+            content = file.read()
+            for website in self.websites:
+                if website in content:
+                    pass
+                else:
+                    line = f'127.0.0.1 \t { website } \n'
+                    file.write(line)
+
+    def Route(self):
+        if self.os_name == 'posix':
+            self.route = '/etc/hosts'
+        elif self.os_name == 'nt':
+            self.route = 'C:\Windows\System32\drivers\etc\hosts'
+        else:
+            print('\033[;31mYour operating system is not supported. Please contact us.')
+
+print("""
+\033[1;33m
+----------------------------------------------------------------
+\033[1;32m
+       __          __  _         _ _       
+       \ \        / / | |       (_) |      
+        \ \  /\  / /__| |__  ___ _| |_ ___ 
+         \ \/  \/ / _ \ '_ \/ __| | __/ _ \\
+          \  /\  /  __/ |_) \__ \ | ||  __/
+           \/  \/ \___|_.__/|___/_|\__\___|
+                                           
+                                           
+         ____  _            _             
+        |  _ \| |          | |            
+        | |_) | | ___   ___| | _____ _ __ 
+        |  _ <| |/ _ \ / __| |/ / _ \ '__|
+        | |_) | | (_) | (__|   <  __/ |   
+        |____/|_|\___/ \___|_|\_\___|_|          
+\033[1;33m
+----------------------------------------------------------------
+\033[;33mEnter a URL or write \'ok\' to close the program.
+""")
+
+website_blocker = WebsiteBlocker()
+website_blocker.Main()
